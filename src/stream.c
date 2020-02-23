fa905d3fee99dda6133f43e93da844978fc6edbb
steven <steve@fancydash.io>
2020-02-23 23:31:40 +0800

    implement string stream methods

--- src/stream.c
+++ src/stream.c
@@ -1,21 +1,59 @@
 #include "stream.h"
 
+typedef enum {
+    FILE_STREAM,
+    STRING_STREAM,
+} cir_stream_type;
+
 typedef struct stream {
-    FILE* source;
+    cir_stream_type type;
     char next_char;
     char current_char;
-    uintmax_t line;
-    uintmax_t column;
+    size_t line;
+    size_t column;
+    size_t pos;
+
+    union {
+        FILE* f_source;
+        char* s_source;
+    };
 } stream;
 
 stream* file_stream_new(FILE* source) {
     stream* s = xmalloc(sizeof(stream));
-    s -> source = source;
+    s -> type = FILE_STREAM;
+    s -> f_source = source;
     s -> current_char = fgetc(source);
     s -> next_char = fgetc(source);
     s -> line = 1;
     s -> column = 1;
     return s;
+}
+
+stream* string_stream_new(char* source) {
+   stream* s = xmalloc(sizeof(stream));
+    s -> type = STRING_STREAM;
+    s -> pos = 2;
+    s -> s_source = source;
+
+    size_t len = strlen(source);
+
+    if (len > 0) {
+        s -> current_char = source[0];
+    } else {
+        s -> current_char = EOF;
+    }
+
+    if (len > 1) {
+        s -> next_char = source[1];
+    } else {
+        s -> next_char = EOF;
+    }
+
+    s -> line = 1;
+    s -> column = 1;
+    return s;
+
 }
 
 void stream_delete(stream* s) {
@@ -43,8 +81,15 @@ char s_next_char(const stream* s) {
 }
 
 void s_read_char(stream* s) {
-    s -> current_char = s -> next_char;
-    s -> next_char = fgetc(s -> source);
+    if (s -> type == FILE_STREAM) {
+        s -> current_char = s -> next_char;
+        s -> next_char = fgetc(s -> f_source);
+   } else {
+        s -> current_char = s -> next_char;
+        s -> next_char = s -> s_source[s -> pos];
+        s -> pos++;
+    }
+
     if (s -> current_char == '\n') {
         s -> line++;
         s -> column = 0;
